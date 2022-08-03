dfa59bdfac9ac2c1df0d27201c2d105a6d073ff4
Yatiesh Mehta <yatiesh.mehta@gmail.com>
2022-08-03 20:59:01 +0200

    Added how to create a tensor-ready Docker image

---
+++ TF_2.x/tensor_test.py
@@ -0,0 +1,16 @@
+import numpy
+import cv2
+import matplotlib
+import tensorflow
+
+print("Hello World")
+
+mat1 = [[1, 2], 
+        [3, 4]]
+        
+mat2 = [[1, 2], 
+        [3, 4]]
+
+mat3 = numpy.dot(mat1, mat2)
+
+print(mat3)
